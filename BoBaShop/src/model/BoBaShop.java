ad57b1a36f9972965719a39412cf5a9f6de3f968
Varot <Varot@LAPTOP-K459C2GB>
2020-04-30 14:23:03 +0700

    BoBaShop เพิ่ม Method

--- BoBaShop/src/model/BoBaShop.java
+++ BoBaShop/src/model/BoBaShop.java
@@ -1,16 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
-/**
- *
- * @author User
- */
-public class BoBaShop {
-    public void cal(){
+//import Exception.ExceedMaxCapacityException;
+//import dbaccess.Stock;
 
+public class BoBaShop {
+private String name;
+//    private CustomerAccount[] customers;
+//    private StaffAccount staff;
+    private Stock stock;
+
+    public BoBaShop(String name) {
+        this.name = name;
+//        this.stock = new Stock(300);
+    }
+    
+    public void addNewMenu(Product p){
+//        stock.insertProduct(p);
+    }
+    
+    public void addNewMenu(Product p,int amount){
+//        stock.insertProduct(p);
+//        restock(p, amount);
+    }
+    
+    public void showMenu(){
+//        stock.showAll();
+    }
+    
+    public void updateStock(Product product, int amount){
+//        stock.update(product, amount);
+    }
+    
+    public void restock(Product product, int amount){
+//        try {
+//            stock.restock(product, amount);
+//        } catch (ExceedMaxCapacityException ex) {
+//            System.out.println("Can't restock because: "+ex.getMessage());
+//        }
     }
 }
