b169154281a50d22221255904d7f965f5c509bb9
User <User@MSI>
2020-04-30 23:36:14 +0700

    delete method

--- BoBaShop/src/testInterface/UserInterface.java
+++ BoBaShop/src/testInterface/UserInterface.java
@@ -11,31 +11,30 @@ import java.util.Scanner;
  *
  * @author User
  */
- import java.util.Scanner; 
+import java.util.Scanner;
 
 public class UserInterface {
-	static Scanner yourChoice=new Scanner(System.in); 
-	static int confirmID;
-	public String page1(){ 
-		String choice="test";
-		
-		System.out.println("1.Sign in");
-		System.out.println("2.Sign up"); 
-		System.out.print("Choose your option:  ");
-		while (!(choice.equals("sign in"))&&!(choice.equals("sign up"))&&!(choice.equals("1"))&&!(choice.equals("2"))){ 																														
-			choice=yourChoice.nextLine();
-			choice=choice.toLowerCase();
-			if(!(choice.equals("sign in"))&&!(choice.equals("sign up"))&&!(choice.equals("1"))&&!(choice.equals("2"))){ 																														
-				System.out.println("You did not enter one of the options"); 
-				System.out.println("Please re-enter either \"1\" or \"2\"");
-																			
-			}
-		} 
-		return choice;
-	}
-	
-	public void page2(){
-		User person= new User();
-		person.getUsername(); 
-	}    
+
+    static Scanner yourChoice = new Scanner(System.in);
+    static int confirmID;
+
+    public String page1() {
+        String choice = "test";
+
+        System.out.println("1.Sign in");
+        System.out.println("2.Sign up");
+        System.out.print("Choose your option:  ");
+        while (!(choice.equals("sign in")) && !(choice.equals("sign up")) && !(choice.equals("1")) && !(choice.equals("2"))) {
+            choice = yourChoice.nextLine();
+            choice = choice.toLowerCase();
+            if (!(choice.equals("sign in")) && !(choice.equals("sign up")) && !(choice.equals("1")) && !(choice.equals("2"))) {
+                System.out.println("You did not enter one of the options");
+                System.out.println("Please re-enter either \"1\" or \"2\"");
+
+            }
+        }
+        return choice;
+    }
+
+    
 }
