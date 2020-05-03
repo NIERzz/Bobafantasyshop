dd877f6a36acf84dce33c3b6a5b49d56dd6a910b
Varot <Varot@LAPTOP-K459C2GB>
2020-05-03 13:28:11 +0700

    Rename คลาส

---
+++ BoBaShop/src/Exception/NoProductException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exception;
+
+/**
+ *
+ * @author User
+ */
+public class NoProductException extends Exception {
+    public NoProductException(String msg) {
+        super(msg);
+    }
+}
